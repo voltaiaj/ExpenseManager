32f3b492e14231be648211981d15e98f3ab0e7ff
voltaiaj <alexander.voltaire@gmail.com>
2016-02-11 04:57:37 -0600

    Modifying filters

--- ExpenseManager.Web/App/expenseSummary/expenseSummaryFilters.js
+++ ExpenseManager.Web/App/expenseSummary/expenseSummaryFilters.js
@@ -2,7 +2,9 @@
     'use strict';
     angular.module('em.expenseSummary')
         .filter('oddFilter', oddFilter)
-        .filter('evenFilter', evenFilter);
+        .filter('evenFilter', evenFilter)
+        .filter('matrixFilter', matrixFilter)
+    ;
 
     function oddFilter() {
         return function(inputArray) {
@@ -26,6 +28,21 @@
                     result.push(inputArray[index]);
                 }
             }
+            return result;
+        }
+    }
+
+    function matrixFilter() {
+        return function (inputArray) {
+            var result = [];
+
+            for (var index in inputArray) {
+                if (inputArray[index] % 2 == 0) {
+                    result.push({ value: inputArray[index], color: 'redbox' });
+                } else if(isNaN(inputArray[index]) == false) {
+                    result.push({ value: inputArray[index], color: 'bluebox' });
+                }
+            }                        
             return result;
         }
     }
