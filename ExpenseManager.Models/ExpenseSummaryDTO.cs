ad38149726f7afd856ef869554e5ef47dc306247
Voltaiaj <Alexander.Voltaire@gmail.com>
2016-04-24 16:59:58 -0500

    Adding IEnumerable<ValueName> Categories to  model

--- ExpenseManager.Models/ExpenseSummaryDTO.cs
+++ ExpenseManager.Models/ExpenseSummaryDTO.cs
@@ -1,17 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using ExpenseManager.Models.Helpers;
 
 namespace ExpenseManager.Models
 {
     public class ExpenseSummaryDTO
     {
         public IEnumerable<string> Labels_Categories { get; set; }
-        public IEnumerable<decimal> Data_Categories { get; set; }         
-        public IEnumerable<string> Labels_Tiers { get; set; } 
-        public IEnumerable<decimal> Data_Tiers { get; set; } 
-        public IEnumerable<Expense> Expenses { get; set; }  
+        public IEnumerable<decimal> Data_Categories { get; set; }
+        public IEnumerable<string> Labels_Tiers { get; set; }
+        public IEnumerable<decimal> Data_Tiers { get; set; }
+        public IEnumerable<Expense> Expenses { get; set; }
+
+        public IEnumerable<ValueName> Categories
+        {
+            get { return EnumHelper.GetItems<Categories>(); }
+        }
     }
 }
