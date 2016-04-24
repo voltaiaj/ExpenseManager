2b4f001d8fbd4cfaec5ba2f7a319aa3c08fc757d
Voltaiaj <Alexander.Voltaire@gmail.com>
2016-04-24 17:02:23 -0500

    Adding dropdown selection for updating Categories

--- ExpenseManager.Web/app/expenseSummary/expenseSummaryController.js
+++ ExpenseManager.Web/app/expenseSummary/expenseSummaryController.js
@@ -9,10 +9,6 @@
         var vm = this;
         vm.title = 'ExpenseSummary';
         vm.expenseToEdit = {};
-        vm.categories = [{ Name: 'Gym', Value: 1 }, { Name: 'Mortgage', Value: 2 }, { Name: 'CarInsurance', Value: 3 },
-            { Name: 'SatelliteTv', Value: 4 }, { Name: 'CellPhone', Value: 5 }, { Name: 'Internet', Value: 6 },
-            { Name: 'Spotify', Value: 7 }, { Name: 'GasForHome', Value: 8}
-        ];
 
         vm.editExpense = editExpense;
         vm.updateExpense = updateExpense;
@@ -21,8 +17,8 @@
 
         function activate() {
             var date = new Date();
-            expenseManagerDataService.getCurrentMonthExpenses(date).then(callSuccess);
-            expenseManagerDataService.getCurrentMonthSummary().then(callSuccess);
+            //expenseManagerDataService.getCurrentMonthExpenses(date).then(callSuccess);
+            dataRefresh();
         }
 
         function callSuccess(response) {
@@ -31,7 +27,12 @@
             vm.data_Categories = response.data.data_Categories;
             vm.labels_Tiers = response.data.labels_Tiers;
             vm.data_Tiers = response.data.data_Tiers;
+            vm.categories = response.data.categories;
             console.log(response.data);
+        }
+
+        function dataRefresh() {
+            expenseManagerDataService.getCurrentMonthSummary().then(callSuccess);
         }
 
         function editExpense(expense) {
@@ -40,6 +41,7 @@
 
         function updateExpense(expenseToUpdate) {
             expenseManagerDataService.updateExpense(expenseToUpdate);
+            dataRefresh();
         }
     }
 })();
